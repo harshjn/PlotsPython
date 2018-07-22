b0de2e841f5c69629ce1ecdbf364aff814959b6d
harshjn <harshjainldh@gmail.com>
2018-07-22 19:07:22 +0530

    Create Multi2AxisPlots.py

---
+++ Multi2AxisPlots.py
@@ -0,0 +1,9 @@
+fig,ax1=plt.subplots()
+ax1.plot(xmat1,Fpot(xmat1),'b')
+ax1.set_ylabel('F_Potential',color='b')
+ax1.tick_params('y',colors='b')
+
+ax2=ax1.twinx()
+ax2.plot(xmat1,np.multiply(-1,Pmat),'r')
+ax2.set_ylabel(' Log(Probability) observed',color='r')
+ax2.tick_params('y',colors='r')
