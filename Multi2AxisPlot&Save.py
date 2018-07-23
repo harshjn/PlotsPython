f3e45cf6cfbfc7632f7e53814de23af1ff645d9d
harshjn <harshjainldh@gmail.com>
2018-07-23 14:55:31 +0530

    Update Multi2AxisPlot&Save.py

--- Multi2AxisPlot&Save.py
+++ Multi2AxisPlot&Save.py
@@ -8,6 +8,5 @@ ax2.plot(xmat1,np.multiply(-1,Pmat),'r')
 ax2.set_ylabel(' Log(Probability) observed',color='r')
 ax2.tick_params('y',colors='r')
 
-
-np.savetxt('myfile.txt', np.column_stack([xmat1,Pmat,F(xmat1)]))
-#np.loadtxt('myfile.txt')
+addSave='C:/Users/Admin/OneDrive/BB30Lab/Project_OT/7.19.18/'
+np.savetxt(addSave+'Type3Pot_analytic.txt', np.column_stack([xmat1,Pmat,F(xmat1)]))
